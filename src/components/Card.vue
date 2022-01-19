7d12fecb2509932ed46775df2fa2e6d32ad9eeee
Максим-home <berez_max@mail.ru>
2022-01-19 17:05:55 +0300

    Разместить данные php на карточках

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="col s12 m6">
-    <div class="card">
-      <div class="card-panel">
-
-        <p>Name</p>
+    <div class="card horizontal blue-grey darken-1">
+      <div class="card-content white-text">
+        <span class="card-title">{{ card_data.title }}</span>
+        <p class="card-description">{{ card_data.description }}</p>
+        <ul class="card-tags">
+          <li class="tag" v-for="tag of card_data.stack" :key="tag">
+            {{ tag }}
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <a class="waves-effect waves-light activator btn-small">Подробней</a>
+      </div>
+      <div class="card-reveal blue-grey darken-1">
+        <span class="card-title white-text">{{ card_data.title }}</span>
+        <p class="white-text">{{card_data.details}}</p>
       </div>
     </div>
   </div>
