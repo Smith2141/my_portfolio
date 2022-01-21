86767a509b1f5adb051683fd4ce72c283d8844dd
Максим-home <berez_max@mail.ru>
2022-01-21 11:41:56 +0300

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div>
+    <div id="app">
+      <router-view />
+    </div>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Footer from './components/common/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Footer
+  }
+}
+</script>
 
 <style lang="css">
 @import '~materialize-css/dist/css/materialize.min.css';
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="page-footer">
+    <div class="footer-copyright">
+      <div class="blue-grey-text text-darken-1">
+        &copy;
+        {{ footer_year }}
+        <a
+          href="https://ru.freepik.com/vectors/background"
+          target="_blank"
+          class="blue-grey-text text-darken-1"
+          >Background вектор создан(а) rawpixel.com - ru.freepik.com</a
+        ><a
+          href="https://www.pngegg.com/en/search?q=programming+language"
+          target="_blank"
+          class="blue-grey-text text-darken-1"
+          >Логотипы взяты с pngegg.com</a
+        >
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      footer_year: new Date().getFullYear()
+    }
+  }
+}
+</script>
